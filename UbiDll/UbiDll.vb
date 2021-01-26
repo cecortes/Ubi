1c98b65e6dcaa1fcb15bc4bda69fbafff791349b
César López Cortés <cesarlopezcortes@hotmail.com>
2021-01-26 16:50:56 -0600

    Function NewAlmaPrima added

--- UbiDll/UbiDll.vb
+++ UbiDll/UbiDll.vb
@@ -4455,6 +4455,68 @@ Public Class Agregar
 
 #End Region
 
+#Region "ALMACEN PRIMA"
+
+    ''' <summary>
+    ''' Realiza la inserción de datos en la tabla alma_prima
+    ''' </summary>
+    ''' <param name="datos"></param>
+    ''' <returns></returns>
+    Public Function NewAlmaPrima(ByVal datos As Datos) As Boolean
+
+        'Privadas
+        Dim estado As Boolean = False
+        Dim con As New Conexion
+
+        'Control excepciones
+        Try
+
+            'Conexión
+            con.Con_Global()
+
+            'Query
+            _adaptador.InsertCommand =
+                New MySqlCommand("INSERT INTO alma_prima 
+                (alma_prima_folio, alma_prima_fecha, alma_prima_cadu, alma_prima_tpo, alma_prima_nom, alma_prima_uni, alma_prima_pack, alma_prima_canti) 
+                VALUES 
+                (@alma_prima_folio,@alma_prima_fecha,@alma_prima_cadu,@alma_prima_tpo,@alma_prima_nom,@alma_prima_uni,@alma_prima_pack,@alma_prima_canti)", con._conexion)
+
+            'Parámetros
+            _adaptador.InsertCommand.Parameters.Add("@alma_prima_folio", MySqlDbType.String, 45).Value = datos.alma_prima_folio
+            _adaptador.InsertCommand.Parameters.Add("@alma_prima_fecha", MySqlDbType.Date).Value = datos.alma_prima_fecha
+            _adaptador.InsertCommand.Parameters.Add("@alma_prima_cadu", MySqlDbType.Date).Value = datos.alma_prima_cadu
+            _adaptador.InsertCommand.Parameters.Add("@alma_prima_tpo", MySqlDbType.String, 45).Value = datos.alma_prima_tpo
+            _adaptador.InsertCommand.Parameters.Add("@alma_prima_nom", MySqlDbType.String, 100).Value = datos.alma_prima_nom
+            _adaptador.InsertCommand.Parameters.Add("@alma_prima_uni", MySqlDbType.String, 45).Value = datos.alma_prima_uni
+            _adaptador.InsertCommand.Parameters.Add("@alma_prima_pack", MySqlDbType.String, 45).Value = datos.alma_prima_pack
+            _adaptador.InsertCommand.Parameters.Add("@alma_prima_canti", MySqlDbType.Int32, 11).Value = datos.alma_prima_canti
+
+            'Insert
+            con._conexion.Open()
+            _adaptador.InsertCommand.Connection = con._conexion
+            _adaptador.InsertCommand.ExecuteNonQuery()
+            estado = True
+
+        Catch ex As MySqlException
+
+            'Error
+            estado = False
+            MsgBox(ex.ToString, MsgBoxStyle.Critical, "UbiSoft by Ubicamatic - 2020(C)")
+
+        Finally
+
+            'Conexión Close
+            con._conexion.Close()
+
+        End Try
+
+        'Estado
+        Return estado
+
+    End Function
+
+#End Region
+
 End Class
 
 Public Class Actualizar
