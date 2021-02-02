0cd52558dfc14b5369db26e98ba04209a8f75753
César López Cortés <cesarlopezcortes@hotmail.com>
2021-02-02 16:39:48 -0600

    Function DelProdAlmaTer added

--- UbiDll/UbiDll.vb
+++ UbiDll/UbiDll.vb
@@ -7879,6 +7879,54 @@ Public Class Eliminar
 
     End Function
 
+    ''' <summary>
+    ''' Se encarga de eliminar un producto de almacén producto terminado por medio de la key recibida como parámetro
+    ''' </summary>
+    ''' <param name="datos"> Resultado del borrado </param>
+    ''' <returns></returns>
+    Public Function DelProdAlmaTer(ByVal datos As Datos) As Boolean
+
+        'Variables Privadas
+        Dim estado As Boolean = False
+
+        Dim con As New Conexion
+
+        'Control de Errores
+        Try
+
+            'Conexión
+            con.Con_Global()
+
+            'Query
+            _adaptador.DeleteCommand = New MySqlCommand("DELETE FROM almater WHERE almater_nom=@almater_nom", con._conexion)
+
+            'Parámetros
+            _adaptador.DeleteCommand.Parameters.Add("@almater_nom", MySqlDbType.String, 100).Value = datos.almanom
+
+            'Delete
+            con._conexion.Open()
+            _adaptador.DeleteCommand.Connection = con._conexion
+            _adaptador.DeleteCommand.ExecuteNonQuery()
+            estado = True
+
+        Catch ex As MySqlException
+
+            'Error
+            estado = False
+            MsgBox(ex.ToString, MsgBoxStyle.Critical, "UbiSoft by Ubicamatic - 2020(C)")
+
+        Finally
+
+            'Conexión Close
+            con._conexion.Close()
+
+        End Try
+
+        'Resultado
+        Return estado
+
+    End Function
+
 #End Region
 
 End Class
